a538099093950092755c6312515df1f809392de5
thiagobcf <thiagobcfonseca@gmail.com>
2023-12-18 10:35:19 -0300

    Cors

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using System.Text;
 using System.Text.Json.Serialization;
 
@@ -19,36 +18,7 @@ builder.Services.AddControllers()
             .ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "apitest", Version = "v1" });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "Header de autorização JWT usando o esquema Bearer.\r\n\r\nInforme 'Bearer'[espaço] e o seu token.\r\n\r\nExamplo: \'Bearer 12345abcdef\'",
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-       {
-          new OpenApiSecurityScheme
-          {
-             Reference = new OpenApiReference
-             {
-                 Type = ReferenceType.SecurityScheme,
-                 Id = "Bearer"
-             }
-          },
-          new string[] {}
-       }
-    });
-});
+builder.Services.AddSwaggerGen();
 
 string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 
@@ -90,6 +60,17 @@ var mappingConfig = new MapperConfiguration(mc =>
 IMapper mapper = mappingConfig.CreateMapper();
 builder.Services.AddSingleton(mapper);
 
+////definir politica cors via atributo
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("PermitirApiRequest",
+//        builder =>
+//        builder.WithOrigins("https://wwwapirequest.io/")
+//     .WithMethods("GET")
+//     );
+//});
+builder.Services.AddCors();
+
 
 var app = builder.Build();// Configure
 
@@ -105,6 +86,13 @@ app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+
+//politica CORS restritiva
+//app.UseCors(opt => opt.
+//    WithOrigins("https://wwwapirequest.io/")
+//     .WithMethods("GET"));
+
+app.UseCors(opt => opt.AllowAnyOrigin());
 
 app.MapControllers();
 app.Run();
